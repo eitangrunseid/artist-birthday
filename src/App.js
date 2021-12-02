05760b9e7355d204de4f6aa5a121bc0b1a6b71c4
eitangrunseid <77745620+eitangrunseid@users.noreply.github.com>
2021-12-02 22:41:39 +0200

    clean cooments

--- src/App.js
+++ src/App.js
@@ -4,11 +4,6 @@ import SearchScreen from "./screens/SearchScreen";
 function App() {
 	return (
 		<div className="App">
-			{/* <h3>
-				if you are interested in knowing the Jewish date of birth of an artist.
-				Search in the search bar for the artist's name
-			</h3> */}
-
 			<SearchScreen />
 		</div>
 	);
